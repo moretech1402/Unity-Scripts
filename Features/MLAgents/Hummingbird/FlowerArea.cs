e4e17e18f9dcba8d05f1c5aac9e262a95d360e0e
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2025-01-25 15:31:37 +0100

    Feat : create find flowers function in FlowerArea

--- Features/MLAgents/Hummingbird/FlowerArea.cs
+++ Features/MLAgents/Hummingbird/FlowerArea.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 /// <summary>Manages a collection of flower plants and attached flowers</summary>
 public class FlowerArea : MonoBehaviour
 {
-    // The diameter of the area where the agent and flowers can be used for observing relative distance from agent to flower
+    /// <summary>The diameter of the area where the agent and flowers can be used for observing relative distance from agent to flower</summary>
     public const float AreaDiameter = 20f;
+
+    [SerializeField] string flowerPlantTag = "FlowerPlant";
 
     // The list of all flower plants in this area (flower plants have multiple flowers)
     List<GameObject> flowerPlants = new();
 
     // A lookup Dictionary for looking up a flower from a nectar collider
-    Dictionary<Collider, Flower> nectarFlowerDictionary = new();
+    Dictionary<Collider, Flower> nectarFlowerMap = new();
 
     /// <summary>The list of all flowers in the flower area</summary>
     public List<Flower> Flowers {get; private set;} = new();
@@ -39,14 +41,43 @@ public class FlowerArea : MonoBehaviour
     /// <param name="collider">The nectar collider</param>
     /// <returns>The matching flower</returns>
     public Flower GetFlowerFromNectar(Collider collider){
-        return nectarFlowerDictionary[collider];
+        return nectarFlowerMap[collider];
     }
 
-    public Flower[] FindChildFlowers(){
+    /// <summary>Recursevily finds all children flowers and flower plants</summary>
+    public Flower[] FindChildFlowers(Transform transform){
+        for(int i = 0; i < transform.childCount; i++){
+            Transform child = transform.GetChild(i);
+
+            if(child.CompareTag(flowerPlantTag)){
+                // Found a flower plant, add to list
+                flowerPlants.Add(child.gameObject);
+
+                // Look for flower plants within this flower plant
+                FindChildFlowers(child);
+            } else {
+                // Not a flower plant, look for flower component
+                
+                if(child.TryGetComponent<Flower>(out var flower))
+                {
+                    // Found a flower, add it to the flowers list
+                    Flowers.Add(flower);
+
+                    // Map nectar collider to flower
+                    nectarFlowerMap.Add(flower.NectarCollider, flower);
+
+                    // Note: there are not flowers that are children of other flowers
+                } else {
+                    // Flower component not found, so check children
+                    FindChildFlowers(child);
+                }
+            }
+        }
+
         return new Flower[0];
     }
 
     private void Start() {
-        FindChildFlowers();
+        FindChildFlowers(transform);
     }
 }
