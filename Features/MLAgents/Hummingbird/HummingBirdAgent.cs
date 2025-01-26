7c6cd0c092fec6b063c7f402df98f45f18d62374
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2025-01-26 09:53:09 +0100

    Feat : create private variables in humming bird agent

--- Features/MLAgents/Hummingbird/HummingBirdAgent.cs
+++ Features/MLAgents/Hummingbird/HummingBirdAgent.cs
@@ -29,4 +29,32 @@ public class HummingBirdAgent : Agent
 
     [Tooltip("Whether this is training mode or gameplay mode")]
     [SerializeField] bool trainingMode;
+
+
+    /// <summary>The rigidbody of the agent</summary>
+    new Rigidbody rigidbody;
+
+    /// <summary>The flower area that the agent is in</summary>
+    FlowerArea flowerArea;
+
+    /// <summary>The nearest flower to the agent</summary>
+    Flower nearestFlower;
+
+    /// <summary>Allows for smoother pitch changes</summary>
+    float smootherPitchChange;
+
+    /// <summary>Allows for smoother yaw changes</summary>
+    float smootherYawChange;
+
+    /// <summary>Maximum angle that the bird can pitch up or down</summary>
+    const float MAX_PITCH_ANGLE = 80;
+
+    /// <summary>Maximum distance from the beak tip to accept nectar collision</summary>
+    const float BEAK_TIP_RADIUS = .008f;
+
+    /// <summary>Whether the agent is frozen (intentionally not flying)</summary>
+    bool frozen;
+
+    /// <summary>The amount of nectar the agent has obtained this episode</summary>
+    public float NectarObtained { get; private set; }
 }
