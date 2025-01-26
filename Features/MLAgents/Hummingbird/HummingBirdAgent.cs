ada9e9a1405f46f5b67de01ab7a2603d26b65954
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2025-01-26 11:12:14 +0100

    Feat : create initialize functions of hummingbird agent

--- Features/MLAgents/Hummingbird/HummingBirdAgent.cs
+++ Features/MLAgents/Hummingbird/HummingBirdAgent.cs
@@ -57,4 +57,50 @@ public class HummingBirdAgent : Agent
 
     /// <summary>The amount of nectar the agent has obtained this episode</summary>
     public float NectarObtained { get; private set; }
+
+
+    public void MoveToSafeRandomPosition(bool inFrontOfFlower)
+    {
+        // ...
+    }
+
+    public void UpdateNearestFlower()
+    {
+        // ...
+    }
+
+    public override void Initialize()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        flowerArea = GetComponentInParent<FlowerArea>();
+
+        // If not training mode, no max step, play forever
+        if(!trainingMode) MaxStep = 0;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // Reset Nectar obtained
+        NectarObtained = 0f;
+
+        // Zero out velocities so that movement stops before a new episode begins
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+
+        // Default to spawning in front of a flower
+        bool inFrontOfFlower = true;
+        if(trainingMode){
+            // Only reset in training when there is one agent per area
+            flowerArea.ResetFlowers();
+
+            // Spawn in front of a flower 50% of the time during training
+            inFrontOfFlower = Random.value > .5f;
+        }
+
+        // Move the agent to a new random position
+        MoveToSafeRandomPosition(inFrontOfFlower);
+
+        // Recalculate the nearest flowers now that the agent has moved
+        UpdateNearestFlower();
+    }
 }
