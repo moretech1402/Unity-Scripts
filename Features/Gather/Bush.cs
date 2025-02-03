d40686ff280fb52c0a0cce7bfb3cf303e849a047
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2025-02-03 10:10:24 +0100

    [feat] create Bush to Gather

---
+++ Features/Gather/Bush.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    [SerializeField] Transform berriesContainer;
+
+    int berriesNumber = 5;
+    readonly HashSet<GameObject> selectedBerries = new();
+
+    /// <summary>
+    /// Choose random number beetween 0 and max berries number
+    /// </summary>
+    /// <returns>Random number</returns>
+    int ChooseRandomNumber() => Random.Range(0, berriesContainer.childCount);
+
+    /// <summary>Assign berries that will be showed</summary>
+    void AssignRenderBerries(){
+        // Choose random berries locations
+        while(selectedBerries.Count < berriesNumber){
+            // Choose random berry
+            int berryIndex = ChooseRandomNumber();
+            var berryGO = berriesContainer.GetChild(berryIndex).gameObject;
+
+            // Add berry to set
+            selectedBerries.Add(berryGO);
+        }
+    }
+
+    #region Render
+
+    /// <summary>Render active berries</summary>
+    void RenderBerries(){
+        // First disable all berries
+        foreach(Transform child in berriesContainer.transform)
+            child.gameObject.SetActive(false);
+        
+        // Active selected berries
+        foreach(var berry in selectedBerries)
+            berry.SetActive(true);
+    }
+    
+    /// <summary>Set berries number and location of these for this bush</summary>
+    void SetBerries(){        
+        // Calculate number of berries
+        berriesNumber = ChooseRandomNumber();
+
+        // Set visual berries
+        AssignRenderBerries();
+        RenderBerries();
+    }
+
+    #endregion
+
+    #region Life Cycle
+
+    private void Awake() {
+        SetBerries();
+    }
+
+    #endregion
+}
