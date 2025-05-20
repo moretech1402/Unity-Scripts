b7c419d91247931581443e856dd59b782e056326
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2025-05-20 13:43:49 +0200

    Fix null reference on actions

--- Features/Core/Input/InputEventBus.cs
+++ Features/Core/Input/InputEventBus.cs
@@ -5,26 +5,26 @@ namespace Core.Input
 {
     public static class InputEventBus
     {
-        public static event Action<Vector2> OnInputMove;
+        public static event Action<Vector2> OnInputMove = delegate { };
         public static void InputMove(Vector2 move) => OnInputMove?.Invoke(move);
 
-        public static event Action<Vector2> OnInputMouse;
+        public static event Action<Vector2> OnInputMouse = delegate { };
         public static void InputMouse(Vector2 move) => OnInputMouse?.Invoke(move);
 
-        public static event Action OnInputAction;
+        public static event Action OnInputAction = delegate { };
         public static void InputAction() => OnInputAction?.Invoke();
 
         public static event Action<bool> OnInputRun = delegate { };
         public static void InputRun(bool run) => OnInputRun?.Invoke(run);
 
         public static event Action OnInputJump = delegate { };
-        public static void InputJump() => OnInputJump();
+        public static void InputJump() => OnInputJump?.Invoke();
 
         public static event Action OnInputMenu = delegate { };
-        public static void InputMenu() => OnInputMenu();
+        public static void InputMenu() => OnInputMenu?.Invoke();
 
         public static event Action OnInputEscape = delegate { };
-        public static void InputEscape() => OnInputEscape();
+        public static void InputEscape() => OnInputEscape?.Invoke();
     }
 
 }
