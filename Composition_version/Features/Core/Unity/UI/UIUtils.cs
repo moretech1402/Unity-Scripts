508e07e823b1ea702d35bc0f7a06024002d7a9bb
MorenoRT1402 <104833360+moretech1402@users.noreply.github.com>
2026-01-03 10:54:14 +0100

    Add CopyRectTransform utility method
    Introduced CopyRectTransform to copy RectTransform properties from one object to another, simplifying UI element duplication and layout consistency.

--- Composition_version/Features/Core/Unity/UI/UIUtils.cs
+++ Composition_version/Features/Core/Unity/UI/UIUtils.cs
@@ -11,5 +11,14 @@ namespace MC.Core.Unity.UI
                 Object.Destroy(child.gameObject);
             }
         }
+
+        public static void CopyRectTransform(RectTransform from, RectTransform to)
+        {
+            to.anchorMin = from.anchorMin;
+            to.anchorMax = from.anchorMax;
+            to.pivot = from.pivot;
+            to.sizeDelta = from.sizeDelta;
+            to.anchoredPosition = from.anchoredPosition;
+        }
     }
 }
